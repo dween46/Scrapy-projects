27de61f87d904ec7f6b0b1cef07f7a2e20375899
Deen Mohammad <dweenmohammed@gmail.com>
2022-10-28 18:00:45 +0600

    item loader added

--- backmarket/backmarket/items.py
+++ backmarket/backmarket/items.py
@@ -4,6 +4,20 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+from w3lib.html import remove_tags
+
+def remove_currency(value):
+    return value.replace('£', '').strip()
+
+def remove_newline(value):
+    return value.replace('\n', '').strip()
+
+def remove_brackets(value):
+    return value.replace('(', '').replace(')', '').strip()
+
+def remove_slashfive(value):
+    return value.replace('/5', '').strip()
 
 
 # class linkItem(scrapy.Item):
@@ -13,13 +27,13 @@ import scrapy
 
 class BackmarketItem(scrapy.Item):
     date= scrapy.Field()
-    product_name =scrapy.Field()
-    product_id = scrapy.Field()
+    product_name =scrapy.Field(input_processor=MapCompose(remove_tags, remove_newline), output_processor=TakeFirst())
+    product_id = scrapy.Field(input_processor=MapCompose(remove_tags, remove_newline), output_processor=TakeFirst())
     link = scrapy.Field()
-    fair = scrapy.Field()
-    good = scrapy.Field()
-    excellent = scrapy.Field()
-    rating = scrapy.Field()
-    reviews = scrapy.Field()
+    fair = scrapy.Field(input_processor=MapCompose(remove_tags, remove_newline, remove_currency), output_processor=TakeFirst())
+    good = scrapy.Field(input_processor=MapCompose(remove_tags, remove_newline, remove_currency), output_processor=TakeFirst())
+    excellent = scrapy.Field(input_processor=MapCompose(remove_tags, remove_newline, remove_currency), output_processor=TakeFirst())
+    rating = scrapy.Field(input_processor=MapCompose(remove_tags, remove_newline, remove_slashfive), output_processor=TakeFirst())
+    reviews = scrapy.Field(input_processor=MapCompose(remove_tags, remove_newline, remove_brackets), output_processor=TakeFirst())
 
 
--- backmarket/backmarket/spiders/backmarket_crawl.py
+++ backmarket/backmarket/spiders/backmarket_crawl.py
@@ -3,7 +3,9 @@ import scrapy
 import pandas as pd
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
 #from scrapy.crawler import CrawlerProcess
+
 
 from ..items import BackmarketItem
 #from ..items import linkItem
@@ -16,53 +18,69 @@ class BackmarketCrawlSpider(scrapy.Spider):
 
     start_urls = ["https://www.backmarket.co.uk/en-gb/l/iphone/aabc736a-cb66-4ac0-a3b7-0f449781ed39"]
 
-    def parse_product(self, response):
+    # def parse_product(self, response):
 
-        print('Here we go!!!!!!________________________________')
+    #     print('Here we go!!!!!!________________________________')
         
-        item = BackmarketItem()
-        #item = {}
+    #     item = BackmarketItem()
+    #     #item = {}
 
-        #div.flex.flex-col.lg\:max-w-\[38rem\] > 
+    #     #div.flex.flex-col.lg\:max-w-\[38rem\] > 
 
-        item['date'] = self.today
-        try:
-            item['product_name'] = response.css('h1::text').get().strip().replace('\n', '')
-        except:
-            item['product_name'] = "Title Not found"
-        try:
-            item['product_id'] = response.css('li.md\:block.text-link-primary.overflow-hidden.flex-shrink.min-w-\[6ch\].hidden > div::text').get().strip().replace('\n', '')
-        except:
-            item['product_id'] = "ID Not found"
+    #     item['date'] = self.today
+    #     try:
+    #         item['product_name'] = response.css('h1::text').get().strip().replace('\n', '')
+    #     except:
+    #         item['product_name'] = "Title Not found"
+    #     try:
+    #         item['product_id'] = response.css('li.md\:block.text-link-primary.overflow-hidden.flex-shrink.min-w-\[6ch\].hidden > div::text').get().strip().replace('\n', '')
+    #     except:
+    #         item['product_id'] = "ID Not found"
 
-        item['link']  = response.url
+    #     item['link']  = response.url
 
-        try:
-            item['fair'] = response.css('li:nth-child(1) > a > div > div.body-2-light.text-center.text-primary-light::text').get().strip().replace('\n', '').replace('£', '')
-        except:
-            item['fair'] = "Sold out"
-        try:
-            item['good']= response.css('li:nth-child(2) > a > div > div.body-2-light.text-center.text-primary-light::text').get().strip().replace('\n', '').replace('£', '')
-        except:
-            item['good'] = "Sold out"
-        try:
-            item['excellent'] = response.css('li:nth-child(3) > a > div > div.body-2-light.text-center.text-primary-light::text').get().strip().replace('\n', '').replace('£', '')
-        except:
-            item['excellent'] = "Sold out"
-        try:
-            item['rating'] = response.css('div.hidden.md\:flex.flex-grow.flex-col > button > div > span::text').get().strip().replace('\n', '')
-        except:
-            item['rating'] = "No data"
-        try:
-            item['reviews'] = response.css('div.hidden.md\:flex.flex-grow.flex-col > button > span::text').get().strip().replace('\n', '')
-        except:
-            item['reviews'] = "No data"
+    #     try:
+    #         item['fair'] = response.css('li:nth-child(1) > a > div > div.body-2-light.text-center.text-primary-light::text').get().strip().replace('\n', '').replace('£', '')
+    #     except:
+    #         item['fair'] = "Sold out"
+    #     try:
+    #         item['good']= response.css('li:nth-child(2) > a > div > div.body-2-light.text-center.text-primary-light::text').get().strip().replace('\n', '').replace('£', '')
+    #     except:
+    #         item['good'] = "Sold out"
+    #     try:
+    #         item['excellent'] = response.css('li:nth-child(3) > a > div > div.body-2-light.text-center.text-primary-light::text').get().strip().replace('\n', '').replace('£', '')
+    #     except:
+    #         item['excellent'] = "Sold out"
+    #     try:
+    #         item['rating'] = response.css('div.hidden.md\:flex.flex-grow.flex-col > button > div > span::text').get().strip().replace('\n', '')
+    #     except:
+    #         item['rating'] = "No data"
+    #     try:
+    #         item['reviews'] = response.css('div.hidden.md\:flex.flex-grow.flex-col > button > span::text').get().strip().replace('\n', '')
+    #     except:
+    #         item['reviews'] = "No data"
 
-        yield item
-        #return item
+    #     yield item
+    #     #return item
+
+    def parse_product(self, response):
+        l = ItemLoader(item=BackmarketItem(), response=response)
+
+        l.add_value('date', self.today)
+        l.add_css('product_name', 'h1::text')
+        l.add_css('product_id', 'li.md\:block.text-link-primary.overflow-hidden.flex-shrink.min-w-\[6ch\].hidden > div::text')
+        l.add_value('link', response.url)
+        l.add_css('fair', 'li:nth-child(1) > a > div > div.body-2-light.text-center.text-primary-light::text')
+        l.add_css('good', 'li:nth-child(2) > a > div > div.body-2-light.text-center.text-primary-light::text')
+        l.add_css('excellent', 'li:nth-child(3) > a > div > div.body-2-light.text-center.text-primary-light::text')
+        l.add_css('rating', 'div.hidden.md\:flex.flex-grow.flex-col > button > div > span::text')
+        l.add_css('reviews', 'div.hidden.md\:flex.flex-grow.flex-col > button > span::text')
+
+        return l.load_item()
 
     def parse_inside_links(self, response):
         inside_links = response.css('div.md\:w-2\/3.lg\:w-1\/2.lg\:flex-1.max-w-full > div > div > div:nth-child(3) > div:nth-child(3)>ul > li >a::attr(href)').getall()
+        
         for new_link in inside_links:
             inside_link = 'https://www.backmarket.co.uk' + new_link
             #print("***************************************************")
