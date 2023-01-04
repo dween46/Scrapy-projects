e93568a0aaf0cea497df08246049b012f2322c41
Deen Mohammad <dweenmohammed@gmail.com>
2023-01-04 17:50:41 +0600

    changing start url

--- newsLink/newsLink/newsLink/spiders/linkCrawl.py
+++ newsLink/newsLink/newsLink/spiders/linkCrawl.py
@@ -5,7 +5,7 @@ from scrapy.linkextractors import LinkExtractor
 class LinkcrawlSpider(CrawlSpider):
     name = 'linkCrawl'
     allowed_domains = ['prothomalo.com']
-    start_urls = ['https://www.prothomalo.com/opinion']
+    start_urls = ['https://www.prothomalo.com/']
 
     rules = (
         Rule(LinkExtractor(), callback='parse_item'),
